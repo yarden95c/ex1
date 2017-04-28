1ac5f60bf8196e63125e696ca75d200965236c97
yarden cohen <yarden95c@gmail.com>
2017-04-28 15:38:14 +0300

    main window + setting we have a bug with the cancel button

---
+++ client/SettingsViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace client
+{
+    class SettingsViewModel : ViewModel
+    {
+        public string ServerIP
+        {
+            get { return Properties.Settings.Default.ServerIP; }
+            set { Properties.Settings.Default.ServerIP = value; }
+        }
+        public int ServerPort
+        {
+            get { return Properties.Settings.Default.ServerPort; }
+            set { Properties.Settings.Default.ServerPort = value; }
+        }
+        public int MazeRows
+        {
+            get { return Properties.Settings.Default.MazeRows; }
+            set { Properties.Settings.Default.MazeRows = value; }
+        }
+        public int MazeCols
+        {
+            get { return Properties.Settings.Default.MazeCols; }
+            set { Properties.Settings.Default.MazeCols = value; }
+        }
+        public void SaveSettings()
+        {
+            Properties.Settings.Default.Save();
+        }
+        public void CancelSettings()
+        {
+            //TODO
+        }
+    }
+}
---
+++ client/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace client
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
